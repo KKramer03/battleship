1c32a03478acbb2e089a1a7e6536951c571066ea
NinjaMKII <kevinjkramer168@gmail.com>
2020-08-05 16:24:44 -0400

    Fixed always restarting no matter the answer

--- battleship2.js
+++ battleship2.js
@@ -1,8 +1,8 @@
 var min = 2
 var max = 5
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 var location2 = getRandom(min, max);
 var location1 = location2 - 1;
 var location3 = location2 + 1;
@@ -13,65 +13,68 @@ var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+var answer;
 
-while (isSunk == false){
+while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 1-6):")
     if (guess < 1 || guess > 6) {
         alert("Please enter a valid cell number!");
-    } 
+    }
     else {
-        guesses = guesses + 1; 
-        }
+        guesses = guesses + 1;
+    }
 
-    if (guess == location1 && hit1 == 0){ 
+    if (guess == location1 && hit1 == 0) {
         alert("HIT!");
-            hits = hits + 1; 
-            hit1 += 1
-                if (hits == 3) {
-                    isSunk = true;
-                    alert("You sank my battleship!"); 
-                }}
-    else if (guess == location1 && hit1 > 0){
+        hits = hits + 1;
+        hit1 += 1
+        if (hits == 3) {
+            isSunk = true;
+            alert("You sank my battleship!");
+        }
+    }
+    else if (guess == location1 && hit1 > 0) {
         alert("You've already hit here!")
     }
-    else if (guess == location2 && hit2 == 0){ 
+    else if (guess == location2 && hit2 == 0) {
         alert("HIT!");
-        hits = hits + 1; 
+        hits = hits + 1;
         hit2 += 1
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!"); 
-            }}
-    else if (guess == location2 && hit2 > 0){
+        if (hits == 3) {
+            isSunk = true;
+            alert("You sank my battleship!");
+        }
+    }
+    else if (guess == location2 && hit2 > 0) {
         alert("You've already hit here!")
-    }   
-    else if (guess == location3 && hit3 == 0){ 
+    }
+    else if (guess == location3 && hit3 == 0) {
         alert("HIT!");
-        hits = hits + 1; 
+        hits = hits + 1;
         hit3 += 1
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!"); 
-            }}
-    else if (guess == location3 && hit3 > 0){
+        if (hits == 3) {
+            isSunk = true;
+            alert("You sank my battleship!");
+        }
+    }
+    else if (guess == location3 && hit3 > 0) {
         alert("You've already hit here!")
     }
     else {
-            alert("MISS");
-         }
+        alert("MISS");
+    }
 }
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
-    "which means your shooting accuracy was " + (3/guesses); 
-    alert(stats);
+    "which means your shooting accuracy was " + (3 / guesses);
+alert(stats);
 
-if(isSunk == true){
+if (isSunk == true) {
     answer = prompt("Want to play again?")
-    if(answer == "yes" || "Yes" || "y" || "Y"){
+    if (answer == "yes" || answer == "Yes" || answer == "y" || answer == "Y") {
         window.location.reload(false);
     }
-    else{
-        alert("Game Over!")
+    else {
+        alert("Game Over!");
     }
 }
 
-    
---
+++ test.js
@@ -0,0 +1,10 @@
+var cars = ["Lamborghini", "Bugatti", "Ferrari"]
+cars.push("Koenigsegg", "Hennessey")
+cars.sort()
+console.log(cars)
+for(i = 0; i < cars.length; i++){
+console.log(cars[i])
+if(i == "Bugatti"){
+    console.log("woooooooooooork")
+}
+}
